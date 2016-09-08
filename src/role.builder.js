4f3322dd0c1b18ab8ef6e0a37e3c6557324f0e93
Chris Curwick <ccurwick@exoronet.net>
2016-09-07 20:47:25 -0700

    a

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,6 +1,8 @@
 var roleBuilder = {
     run: function (creep) {
-
+        if (!creep.memory.assignedNode) {
+            creep.memory.assignedNode = 0;
+        }
         if (creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
             creep.say('harvesting');
@@ -22,14 +24,8 @@ var roleBuilder = {
             }
             else {
                 var sources = creep.room.find(FIND_SOURCES);
-                for (var source in sources) {
-                    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                        var moved = creep.moveTo(source);
-                        console.log("harvester able to move: " + moved == OK);
-                        if (moved == OK) {
-                            break;
-                        }
-                    }
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[creep.memory.assignedNode]);
 
                 }
             }
