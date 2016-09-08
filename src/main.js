4ae13235c1142bbdefb5e551c9458437446b17ae
Chris Curwick <ccurwick@exoronet.net>
2016-09-07 19:56:13 -0700

    a

--- src/main.js
+++ src/main.js
@@ -26,9 +26,8 @@ module.exports.loop = function () {
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
 
-    if (harvesters.length < 2) {
+    if (harvesters.length < 1) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, { role: 'harvester' });
         console.log('Spawning new harvester: ' + newName);
     }
