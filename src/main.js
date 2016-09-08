65662030f333b4383bed33979a69284e95bce6d1
Chris Curwick <ccurwick@exoronet.net>
2016-09-07 19:50:42 -0700

    a

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+    
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if (harvesters.length < 2) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, { role: 'harvester' });
+        console.log('Spawning new harvester: ' + newName);
+    }
+
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         switch (creep.memory.role) {
