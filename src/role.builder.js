86782b1d049b41be3a0e68840ae0909139a652d3
Chris Curwick <chriscur@microsoft.com>
2016-09-08 08:42:37 -0700

    a

--- src/role.builder.js
+++ src/role.builder.js
@@ -22,7 +22,6 @@ var roleBuilder = {
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
-
             }
         }
     }
