b676812564cfefccffe011e96cf4294ca0db41f6
Chris Curwick <chriscur@microsoft.com>
2016-09-09 08:05:14 -0700

    a

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.VERBOSE = true;
-exports.MAX_HARVESTERS_PER_SOURCE = 4;
-exports.USE_PATHFINDER = true;
-exports.DEFAULT_MIN_LIFE_BEFORE_NEEDS_REFILL = 700;
--- src/shared/memoryManager.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function loadMemory() {
-    this.memory = Memory;
-}
-exports.loadMemory = loadMemory;
